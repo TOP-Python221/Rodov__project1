747de4f71351441a791b51d2afd54453dd7b5edb
RodovR <rodovroman@yandex.ru>
2022-09-09 16:18:37 +0300

    Перенесена функция get_player_name в файл gameset.py

--- gameset.py
+++ gameset.py
@@ -1,5 +1,5 @@
 """Дополнительный модуль: подготовка игрового процесса."""
-
+import config
 # глобальные переменные модуля gameset
 DIM = 3
 RANGE = range(DIM)
@@ -9,12 +9,22 @@ TURNS = [ ['X', 'O', 'O'],
           ['X', 'X', ' '],
           [' ', 'O', ' '] ]
 
-def get_player_name() -> None:
-    """Запрашивает имя игрока и проверяет присутствие этого имени в глобальной переменной статистики, добавляет имя в глобальную переменную текущих игроков."""
-    # stdin -> name
-    # if name not in config.STATS:
-    #     new_player(name)
-    # name -> PLAYERS
+"""Запрос имени игрока и передача введённого значения в глобальную переменную STATS"""
+def get_player_name(name) -> None:
+    """Объявление глобальной переменной STATS в локальной функций get_player_name"""
+    """Запрашивает имя игрока. """
+    while True:
+        player_name = input('Введите имя пользователя: ')
+        if player_name:
+            break
+        else:
+            print('Введите не пустую строку!')
+    """И проверяет присутствие этого имени в глобальной переменной статистики."""
+    if player_name not in config.STATS:
+        """Создаёт запись о новом игроке в глобальной переменной статистики."""
+        config.STATS[player_name] = {'training': True,
+                     'stats': {'wins': 0, 'ties': 0, 'fails': 0}}
+    config.PLAYERS.append(player_name)
 
 
 def game_mode() -> str:
